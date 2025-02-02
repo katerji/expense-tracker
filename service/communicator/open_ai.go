76bc15a89846dc6775474d51a5e2052fcff6effb
katerji.abedalaziz@gmail.com <katerji.abedalaziz@gmail.com>
2025-02-02 13:18:11 +0400

    chore: implement basic implementation for open ai communicator

---
+++ service/communicator/open_ai.go
@@ -0,0 +1,145 @@
+package communicator
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type openAI struct{}
+
+type openAIResult struct {
+	content   string
+	hasResult bool
+}
+
+func (o openAI) chat(ctx context.Context, message string) openAIResult {
+	const url = "https://api.openai.com/v1/chat/completions"
+	jsonBody, err := json.Marshal(defaultOpenAIRequestWithMessage(message))
+	if err != nil {
+		//TODO add logs
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		//TODO add logs
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+	o.setOpenAIChatRequestHeaders(req)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		//TODO add logs
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		//TODO add logs
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+
+	if resp.StatusCode != 200 {
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+
+	bodyAsMap := make(map[string]any)
+	err = json.Unmarshal(body, &bodyAsMap)
+	if err != nil {
+		//TODO add logs
+		return openAIResult{
+			hasResult: false,
+		}
+	}
+	return openAIResult{
+		content:   "ok",
+		hasResult: true,
+	}
+
+}
+
+func (o openAI) getAuthorizationToken() string {
+	return ""
+}
+
+func (o openAI) setOpenAIChatRequestHeaders(request *http.Request) {
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", o.getAuthorizationToken()))
+	request.Header.Set("Content-Type", "application/json")
+}
+
+type openAIRequest struct {
+	Model            string                 `json:"model"`
+	Messages         []openAIRequestMessage `json:"messages"`
+	ResponseFormat   openAIRequestFormat    `json:"response_format"`
+	Temperature      float64                `json:"temperature"`
+	MaxTokens        int                    `json:"max_completion_tokens"`
+	TopP             float64                `json:"top_p"`
+	FrequencyPenalty float64                `json:"frequency_penalty"`
+	PresencePenalty  float64                `json:"presence_penalty"`
+}
+
+type openAIRequestMessage struct {
+	Role    string                 `json:"role"`
+	Content []openAIRequestContent `json:"content"`
+}
+
+type openAIRequestContent struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type openAIRequestFormat struct {
+	Type string `json:"type"`
+}
+
+func defaultOpenAIRequestWithMessage(message string) openAIRequest {
+	const (
+		model               = "gpt-4o-mini"
+		role                = "user"
+		contentType         = "text"
+		responseFormat      = "json_object"
+		temperature         = 1
+		maxCompletionTokens = 2048
+		topP                = 1
+		frequencyPenalty    = 0
+		presencePenalty     = 0
+	)
+	return openAIRequest{
+		Model: model,
+		Messages: []openAIRequestMessage{
+			{
+				Role: role,
+				Content: []openAIRequestContent{
+					{
+						Type: contentType,
+						Text: message,
+					},
+				},
+			},
+		},
+		ResponseFormat: openAIRequestFormat{
+			Type: responseFormat,
+		},
+		Temperature:      temperature,
+		MaxTokens:        maxCompletionTokens,
+		TopP:             topP,
+		FrequencyPenalty: frequencyPenalty,
+		PresencePenalty:  presencePenalty,
+	}
+}
